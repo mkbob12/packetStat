d8ed2491da1a367ffb0708499182c2b2538100fc
soooochan <nowee018@naver.com>
2024-02-20 13:47:12 +0900

    tcp/ udp port 번호

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # packetStat
 Report Packet Stat
 pcap file로부터 packet을 읽어서 IP별 송신 패킷 갯수, 수신 패킷 갯수, 송신 패킷 바이트, 수신 패킷 바이트를 출력
+
+# 실행 방법
+g++ packet-stat.cpp -o packet-stat -lpcap
+
+syntax : packet-stat <pcap file>
+sample : packet-stat test.pcap
--- packet-stat.cpp
+++ packet-stat.cpp
@@ -3,6 +3,9 @@
 #include <netinet/ip.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <netinet/tcp.h> 
+#include <netinet/udp.h> 
 #include <string.h>
 
 #define MAX_IPS 1000
@@ -39,32 +42,67 @@ void update_stats(uint32_t ip, int send_bytes, int receive_bytes) {
     ip_count++;
 }
 
+void print_mac(const uint8_t* source_mac, const uint8_t* dest_mac) {
+    printf("Ethernet Soruce MAC:");
+    for(int i = 0; i < 6; i++){
+        if(i == 5){
+            printf("%02x\n", source_mac[i]);
+            break;
+        }
+        printf("%02x:", source_mac[i]);
+    }
+    printf("Ethernet Destination MAC: ");
+    for(int i = 0; i < 6; i++){
+         if(i == 5){
+            printf("%02x\n", source_mac[i]);
+            break;
+        }
+        printf("%02x:", dest_mac[i]);
+    }
+}
+
+void print_ip(char* source_ip_str, char* dest_ip_str, int packet_len) {
+    printf("Source IP: %s\n", source_ip_str);
+    printf("Destination IP: %s\n", dest_ip_str);
+    printf("IP Packet Length: %d\n", packet_len);
+    printf("================================\n");
+}
+
+void print_transport(int tcp_sport, int tcp_dport, int udp_sport, int udp_dport){
+    printf("TCP Source Port %d \n",tcp_sport);
+    printf("TCP Destination Port %d \n",tcp_dport);
+    printf("UDP Source Port %d \n",udp_sport);
+    printf("UDP Destination Port %d \n",udp_dport);
+    printf("================================\n");
+}
+
 void packet_handler(unsigned char *user, const struct pcap_pkthdr *pkthdr, const unsigned char *packet) {
-    struct ip *ip_header = (struct ip *)(packet + 14);
+    struct ether_header *ether_header = (struct ether_header *)packet;
+    struct ip *ip_header = (struct ip *)(packet + sizeof(struct ether_header));
+    struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ether_header) + sizeof(struct ip));
+    struct udphdr *udp_header = (struct udphdr *)(packet + sizeof(struct ether_header) + sizeof(struct ip));
 
     if (ip_header->ip_v == 4) {
         uint32_t source_ip = ip_header->ip_src.s_addr;
         uint32_t dest_ip = ip_header->ip_dst.s_addr;
 
-         char source_ip_str[INET_ADDRSTRLEN];
+        char source_ip_str[INET_ADDRSTRLEN];
         char dest_ip_str[INET_ADDRSTRLEN];
 
         inet_ntop(AF_INET, &(ip_header->ip_src), source_ip_str, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(ip_header->ip_dst), dest_ip_str, INET_ADDRSTRLEN);
 
-        printf("Source IP: %s\n", source_ip_str);
-        printf("Destination IP: %s\n", dest_ip_str);
-        printf("Packet Length: %d\n", pkthdr->len); 
-        printf("================================\n");
+        print_mac(ether_header->ether_shost, ether_header->ether_dhost);
+        print_ip(source_ip_str, dest_ip_str, pkthdr->len);
 
-        update_stats(source_ip, pkthdr->len, 0); // 출발지니깐 송신 패킷 길이만 + 
-        update_stats(dest_ip, 0, pkthdr->len);   // 목적지니깐 수신 패킷만 길이만 + 
-    
+        print_transport(ntohs(tcp_header->th_sport), ntohs(tcp_header->th_dport), ntohs(udp_header->uh_sport),  ntohs(udp_header->uh_dport));
+
+        update_stats(source_ip, pkthdr->len, 0);
+        update_stats(dest_ip, 0, pkthdr->len);
     }
 }
 
 int main(int argc, char *argv[]) {
-    // ... (same as your existing code)
     if (argc != 2) {
         printf("Usage: %s <pcap_file>\n", argv[0]);
         return 1;
